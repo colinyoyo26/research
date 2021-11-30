6bbc707201f892fd37425f6156f882b126a6ca09
colinyoyo26 <colinyoyo26@gmail.com>
2021-12-01 04:15:00 +0800

    Add option to download imagenet

--- script/get_require.py
+++ script/get_require.py
@@ -2,6 +2,7 @@ import os
 import pip
 import tarfile
 import subprocess
+import argparse
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_path + '/..')
@@ -13,22 +14,30 @@ if failed:
     print('fail to install requirements.txt')
     exit(1)
 
-if not os.path.exists('images'):
-    os.mkdir('images')
-os.chdir('images')
+def get_imagenet():
+    if not os.path.exists('images'):
+        os.mkdir('images')
+    os.chdir('images')
 
-img_tar = 'ILSVRC2012_img_val.tar'
-devkit_tar = 'ILSVRC2012_devkit_t12.tar.gz'
-files = os.listdir('.')
+    img_tar = 'ILSVRC2012_img_val.tar'
+    devkit_tar = 'ILSVRC2012_devkit_t12.tar.gz'
+    files = os.listdir('.')
 
-if not img_tar in files:
-    subprocess.run(['wget', 'https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar'])
-    subprocess.run(['wget', 'https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz'])
+    if not img_tar in files:
+        subprocess.run(['wget', 'https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar'])
+        subprocess.run(['wget', 'https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz'])
 
-if len(files) < 3:
-    with tarfile.open(img_tar) as f:
-        tars = f.getnames()
-        f.extractall()
+    if len(files) < 3:
+        with tarfile.open(img_tar) as f:
+            tars = f.getnames()
+            f.extractall()
 
-    with tarfile.open(devkit_tar) as f:
-        f.extractall()
+        with tarfile.open(devkit_tar) as f:
+            f.extractall()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--imagenet', type=bool, default=False)
+imagenet = vars(parser.parse_args())['imagenet']
+
+if imagenet:
+    get_imagenet()
