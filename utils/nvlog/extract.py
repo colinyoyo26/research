0cfebd4457d5717b8409849c01e3d6473c28c253
colinyoyo26 <colinyoyo26@gmail.com>
2022-01-15 01:58:07 +0800

    Extract additional information from log file

--- utils/nvlog/extract.py
+++ utils/nvlog/extract.py
@@ -14,8 +14,18 @@ def _get_kernel_lines(log_file: str):
     is_valid = lambda line : 'NVIDIA' in line and not 'CUDA' in line
     return [ line for line in lines if is_valid(line) ]
 
+def _get_time_scale(log_file: str):
+    lines = open(log_file).readlines()
+    i = 0
+    while lines[i][0] in ['=', '\"']:
+        i += 1
+    units = lines[i].split(',')
+    scale_dict = {'s': 1e6, 'ms': 1e3, 'us': 1.}
+    return (scale_dict[units[0]], scale_dict[units[1]])
+
 def extract_kernel_tf(log_file: str, sm_log_file: str):
     log_lines = _get_kernel_lines(log_file)
+    time_scales = _get_time_scale(log_file)
     sm_log_lines = _get_kernel_lines(sm_log_file)
 
     _assert_same_kernels(log_lines, sm_log_lines)
@@ -23,10 +33,12 @@ def extract_kernel_tf(log_file: str, sm_log_file: str):
     extracted_lines = []
     for dur_line, sm_line in zip(log_lines, sm_log_lines):
         kernel_name = dur_line.rstrip('\n').split('\"')[-2]
-        dur = dur_line.split(',')[1]
+        stream_id = dur_line.split(',')[-3]
+        start_time = float(dur_line.split(',')[0]) * time_scales[0]
+        dur = float(dur_line.split(',')[1]) * time_scales[1]
         sm_efficiency = sm_line.split(',')[-2]
         achieved_occupancy = sm_line.split(',')[-1]
-        extracted_lines.append(f'\"{kernel_name}\"${dur}${sm_efficiency}${achieved_occupancy}')
+        extracted_lines.append(f'\"{kernel_name}\"${stream_id}${start_time}${dur}${sm_efficiency}${achieved_occupancy}')
 
     extracted_file = log_file.rstrip('log') + 'extracted.log'
     with open(extracted_file, 'w') as fout:
