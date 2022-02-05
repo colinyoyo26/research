8f582747d2044b53275c624f6b5d4960edc789ed
colinyoyo26 <colinyoyo26@gmail.com>
2022-02-05 21:13:27 +0800

    Support multi-stream benchmark

--- utils/nvlog/extract.py
+++ utils/nvlog/extract.py
@@ -3,10 +3,10 @@ def _assert_same_kernels(logs1: list, logs2: list):
         print(f'file length are not the same {len(logs1)} != {len(logs2)}'))
     
     for i in range(len(logs1)):
-        kernel_name1 = logs1[i].rstrip('\n').split('\"')[-2]
-        kernel_name2 = logs2[i].rstrip('\n').split('\"')[-2]
+        kernel_name1 = logs1[i][0]
+        kernel_name2 = logs2[i][0]
         assert(kernel_name1 == kernel_name2 or \
-            print(f'kernels are not the same, line: {i}'))
+            print(f'kernels are not the same, kernel: {kernel_name1}, {kernel_name2}'))
 
 def _get_kernel_lines(log_file: str):
     lines = open(log_file).readlines()
@@ -23,21 +23,46 @@ def _get_time_scale(log_file: str):
     scale_dict = {'s': 1e6, 'ms': 1e3, 'us': 1.}
     return (scale_dict[units[0]], scale_dict[units[1]])
 
+def _extract_log_lines(lines: str, time_scales):
+    extracted = []
+    for line in lines:
+        kernel_name = line.rstrip('\n').split('\"')[-2]
+        stream_id = line.split(',')[-3]
+        start_time = float(line.split(',')[0]) * time_scales[0]
+        dur = float(line.split(',')[1]) * time_scales[1]
+        extracted.append((kernel_name, stream_id, start_time, dur))
+    return extracted
+
+def _extract_metric_lines(lines: str):
+    extracted = []
+    for line in lines:
+        kernel_name = line.rstrip('\n').split('\"')[-2]
+        sm_efficiency = line.split(',')[-2]
+        achieved_occupancy = line.split(',')[-1]
+        extracted.append((kernel_name, sm_efficiency, achieved_occupancy))
+    return extracted
+
 def extract_kernel_tf(log_file: str, sm_log_file: str):
     log_lines = _get_kernel_lines(log_file)
     time_scales = _get_time_scale(log_file)
     sm_log_lines = _get_kernel_lines(sm_log_file)
 
+    log_lines = _extract_log_lines(log_lines, time_scales)
+    sm_log_lines = _extract_metric_lines(sm_log_lines)
+
+    log_lines = sorted(log_lines, key=lambda x : x[0])
+    sm_log_lines = sorted(sm_log_lines, key=lambda x : x[0])
+
     _assert_same_kernels(log_lines, sm_log_lines)
 
     extracted_lines = []
-    for dur_line, sm_line in zip(log_lines, sm_log_lines):
-        kernel_name = dur_line.rstrip('\n').split('\"')[-2]
-        stream_id = dur_line.split(',')[-3]
-        start_time = float(dur_line.split(',')[0]) * time_scales[0]
-        dur = float(dur_line.split(',')[1]) * time_scales[1]
-        sm_efficiency = sm_line.split(',')[-2]
-        achieved_occupancy = sm_line.split(',')[-1]
+    for log_line, sm_line in zip(log_lines, sm_log_lines):
+        kernel_name = log_line[0]
+        stream_id = log_line[1]
+        start_time = log_line[2]
+        dur = log_line[3]
+        sm_efficiency = sm_line[1]
+        achieved_occupancy = sm_line[2]
         extracted_lines.append(f'\"{kernel_name}\"${stream_id}${start_time}${dur}${sm_efficiency}${achieved_occupancy}')
 
     extracted_file = log_file.rstrip('log') + 'extracted.log'
--- utils/nvlog/process.py
+++ utils/nvlog/process.py
@@ -1,13 +1,27 @@
 def process_log(extracted_file: str):
     lines = open(extracted_file).readlines()
 
-    active_count = 0
-    total_time = 0
+    utilization_varieties = []
     for line in lines:
-        duration = float(line.split('$')[1])
-        sm_efficiency = float(line.split('$')[2])
-        achieved_occupacy = float(line.split('$')[3])
-        active_count = active_count + duration * sm_efficiency * achieved_occupacy
-        total_time = total_time + duration
+        _, stream_id, start_time, duration, sm_efficiency, achieved_occupacy = line.split('$')
+        start_time = float(start_time)
+        end_time = start_time + float(duration)
+        utilization = float(sm_efficiency) * float(achieved_occupacy)
+        utilization_varieties.append((start_time, utilization))
+        utilization_varieties.append((end_time, -utilization))
     
-    return active_count / total_time
+    utilization_varieties = sorted(utilization_varieties)
+    utilization = 0.
+    weighted_utilization = 0.
+    total_time = 0.
+    overlaps = 0
+    prev_time = utilization_varieties[0][0]
+    for time, variety in utilization_varieties:
+        if overlaps > 0:
+            weighted_utilization += utilization * (time - prev_time)
+            total_time += time - prev_time
+        overlaps += 1 if variety > 0 else -1
+        utilization += variety 
+        prev_time = time
+
+    return weighted_utilization / total_time, total_time
