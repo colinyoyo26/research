2229be206154e7f3d68f9ae51d59a926475dac10
colinyoyo26 <colinyoyo26@gmail.com>
2022-02-11 02:45:19 +0800

    Modify implementation of stream assignment

--- stream_assignment/stream_assignment.py
+++ stream_assignment/stream_assignment.py
@@ -4,46 +4,50 @@ import argparse
 from collections import deque
 from collections import defaultdict
 
-class StreamAssigner:
-    def __init__(self, json_dict):
-        self.json_dict = json_dict
-        self.arg_nodes = json_dict['arg_nodes']
-        self.num_node = len(json_dict['nodes'])
-        self.adjList = [[] for _ in range(self.num_node)]
-        self.inDegree = [0] * self.num_node
-        for cur_id in range(self.num_node):
-            inputs = json_dict['nodes'][cur_id]['inputs']
-            self.inDegree[cur_id] = len(inputs)
-            for input_id, _, _ in inputs:
-                self.adjList[input_id].append(cur_id)
-        self.assign_stream()
 
-    def get(self):
-        return self.res
+def buildGraph(json_dict):
+    num_node = len(json_dict['nodes'])
+    adjList = [[] for _ in range(num_node)]
+    inDegree = [0] * num_node
+    for cur_id in range(num_node):
+        inputs = json_dict['nodes'][cur_id]['inputs']
+        inDegree[cur_id] = len(inputs)
+        for input_id, _, _ in inputs:
+            adjList[input_id].append(cur_id)
+    return adjList, inDegree
 
-    def assign_stream(self):
-        curLevel = deque(self.arg_nodes)
-        assignment = [-1] * self.num_node
-        while len(curLevel):
-            size = len(curLevel)
-            for i in range(size):
-                cur_id = curLevel.popleft()
-                assignment[cur_id] = i
-                for output_id in self.adjList[cur_id]:
-                    self.inDegree[output_id] -= 1
-                    if self.inDegree[output_id] == 0:
-                        curLevel.append(output_id)
+def default_assign(json_dict, adjList, inDegree):
+    arg_nodes = json_dict['arg_nodes']
+    num_node = len(json_dict['nodes'])
+    curLevel = deque(arg_nodes)
+    assignment = [-1] * num_node
+    while len(curLevel):
+        size = len(curLevel)
+        for i in range(size):
+            cur_id = curLevel.popleft()
+            assignment[cur_id] = i
+            for output_id in adjList[cur_id]:
+                inDegree[output_id] -= 1
+                if inDegree[output_id] == 0:
+                    curLevel.append(output_id)
+    return assignment
+
+def assign_stream(json_dict, assign_func):
+    num_node = len(json_dict['nodes'])
+    adjList, inDegree = buildGraph(json_dict)
+
+    assignment = assign_func(json_dict, adjList, inDegree)
+    
         
-        self.res = defaultdict(lambda: [])
-        for cur_id in range(self.num_node):
-            key = 'attr'
-            if not key in self.json_dict['nodes'][cur_id].keys():
-                key = 'attrs'
-            if key in self.json_dict['nodes'][cur_id].keys():
-                func_name = self.json_dict['nodes'][cur_id][key]['func_name']
-                self.res[func_name].append(assignment[cur_id])
-        
-
+    res = defaultdict(lambda: [])
+    for cur_id in range(num_node):
+        key = 'attr'
+        if not key in json_dict['nodes'][cur_id].keys():
+            key = 'attrs'
+        if key in json_dict['nodes'][cur_id].keys():
+            func_name = json_dict['nodes'][cur_id][key]['func_name']
+            res[func_name].append(assignment[cur_id])
+    return res    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -55,9 +59,9 @@ if __name__ == '__main__':
 
     f = open(json_path)
     json_dict = json.load(f)
-    assigner = StreamAssigner(json_dict)
-    res_json = json.dumps(assigner.get(), indent=2)
+    res = assign_stream(json_dict, default_assign)
+    res_json = json.dumps(res, indent=2)
 
-    path = './stream_assignment/assignment.json'
+    path = os.path.dirname(os.path.abspath(__file__)) + '/assignment.json'
     #path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
     open(path, 'w').write(res_json)
