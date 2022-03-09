6d65e94200df8e6d181fd21b143b5a6e3953795a
colinyoyo26 <colinyoyo26@gmail.com>
2022-03-10 03:42:55 +0800

    Fix kernel_info bug

--- utils/nvlog/info.py
+++ utils/nvlog/info.py
@@ -6,7 +6,7 @@ def get_kernel_info(extracted_file: str):
     visited = set()
 
     for line in lines:
-        kernel_name, stream_id, start_time, duration, sm_efficiency, achieved_occupacy = line.split('$')
+        kernel_name, _, _, duration, sm_efficiency, achieved_occupacy = line.split('$')
         kernel_name = kernel_name[1: -1]
         if kernel_name in visited:
             continue
@@ -14,15 +14,18 @@ def get_kernel_info(extracted_file: str):
         
         duration = float(duration)
         utilization = float(sm_efficiency) * float(achieved_occupacy)
+        
+        assert 0 < utilization <= 100 or print(kernel_name, utilizations, durations)
 
         kernel_name = kernel_name[: len(kernel_name) - 8]
-        kernel_info[kernel_name][0].append(duration)
-        kernel_info[kernel_name][1].append(utilization)
+        kernel_info[kernel_name][0].append(utilization)
+        kernel_info[kernel_name][1].append(duration)
 
     for kernel_name in kernel_info:
         utilizations, durations = kernel_info[kernel_name]
         duration = sum(durations)
         utilization = sum([u * d for u, d in zip(utilizations, durations)]) / duration
+        assert 0 < utilization <= 100 or print(kernel_name, utilizations, durations)
         kernel_info[kernel_name] = (utilization, duration)
 
     return kernel_info
