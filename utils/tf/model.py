16536977e0c008bbfacf5be5ab1f87e488a4e211
colinyoyo26 <colinyoyo26@gmail.com>
2022-03-28 13:27:30 +0800

    Support open source models

--- utils/tf/model.py
+++ utils/tf/model.py
@@ -1,11 +1,13 @@
-from tensorflow.keras.applications import NASNetMobile, EfficientNetB0, MobileNet, ResNet50
+from tensorflow.keras.applications import NASNetMobile, EfficientNetB0, MobileNetV3Small, ResNet50
+from classification_models.tfkeras import Classifiers
 
 def select_model(model_name: str):
     models = {'NASNetMobile': NASNetMobile,
               'EfficientNetB0': EfficientNetB0,
-              'MobileNet': MobileNet,
-              'ResNet50': ResNet50}
+              'MobileNet': MobileNetV3Small,
+              'ResNet50': ResNet50,
+              'ResNeXt50': Classifiers.get('resnext50')[0]}
 
     model = models.get(model_name)
     assert(model)
-    return model()
+    return model(include_top=True, input_shape=(224, 224, 3), weights='imagenet')
