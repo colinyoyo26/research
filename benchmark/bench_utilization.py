074a0de7e5d5362ed3eb8c73f9676283160dbac0
colinyoyo26 <colinyoyo26@gmail.com>
2022-06-28 00:03:35 +0800

    Evaluate `fixed achieved occupancy`

--- benchmark/bench_utilization.py
+++ benchmark/bench_utilization.py
@@ -3,21 +3,23 @@ import sys
 import os
 import json
 import pandas as pd
+from collections import defaultdict
 
 ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 sys.path.append(ROOT_PATH)
 from utils import nvlog
+from utils.sched import Graph
 
 #model_names = ['NASNetMobile', 'ResNeXt50', 'Ensemble[NASNetMobile_ResNeXt50]', 'Ensemble[NASNetMobile_ResNet50]']
 model_names = ['NASNetMobile', 'ResNeXt50', 'Ensemble[NASNetMobile_NASNetMobile]', 'Ensemble[NASNetMobile_ResNeXt50]', 
                'Ensemble[NASNetMobile_ResNet50]', 'Ensemble[ResNeXt50_ResNeXt50]', 'Ensemble[ResNeXt50_ResNet50]',
                'Ensemble[ResNet50_ResNet50]', 'Ensemble[NASNetMobile_ResNeXt50_ResNet50]']
-model_names = model_names[7:8]
+model_names = model_names
 #model_names = ['NASNetMobile', 'Ensemble[ResNeXt50_ResNeXt50]', 'Ensemble[NASNetMobile_NASNetMobile]', 'Ensemble[NASNetMobile_ResNeXt50_ResNet50]']
 
 batch_sizes = [8]
 compilers = ['tvm']
-tvm_assign_methods = ['bfs', 'method2']
+tvm_assign_methods = ['default', 'bfs']
 nr_inputs = 1
 
 def doit(compiler, model_name, tvm_assign_method, batch_size, res_file):
@@ -56,12 +58,13 @@ def doit(compiler, model_name, tvm_assign_method, batch_size, res_file):
     subprocess.run(command, capture_output=True)
     subprocess.run(ncu_stats_command)
     
-    write_metrics(log_file + '.log', log_launch_file, res_file, label)
+    tvm_cache = f'./tvm_cache/{model_name}_{batch_size}'
+    write_metrics(tvm_cache, log_file + '.log', log_launch_file, res_file, label)
 
-def write_metrics(log_file, log_launch_file, res_file, res_entry):
+def write_metrics(tvm_cache, log_file, log_launch_file, res_file, res_entry):
     res_json = json.load(open(res_file, 'r'))
     log_launch_table = pd.read_table(log_launch_file, skiprows=[0], sep=',', 
-        usecols=[4, 9, 11], names=['kernel_name', 'metric_name', 'value']) # per kernel log
+        usecols=[4, 9, 10, 11], names=['kernel_name', 'metric_name', 'unit', 'value']) # per kernel log
     log_table = pd.read_table(log_file, skiprows=[0], sep=',', 
         usecols=[9, 10, 11], names=['metric_name', 'unit', 'value']) # model log
 
@@ -76,21 +79,55 @@ def write_metrics(log_file, log_launch_file, res_file, res_entry):
         'Duration': 'duration(ms)'
     }
 
+    unit_trans = {'second': 1e3, 'msecond': 1, 'usecond': 1e-3, 'nsecond': 1e-6}
+
+    if res_entry not in res_json.keys():
+        res_json[res_entry] = {}
+
     for i in range(len(log_table)):
         metric_name = log_table.metric_name[i]
         unit = log_table.unit[i]
         value = log_table.value[i]
         if metric_name in keys.keys():
             key = keys[metric_name]
-            if key == 'duration':
-                value = {'second': 1e3, 
-                         'msecond': 1, 
-                         'usecond': 1e-3, 
-                         'nsecond': 1e-6}[unit] * float(value)
+            if key == 'duration(ms)':
+                value = unit_trans[unit] * float(value)
             res_json[res_entry][key] = value
             
-
+    graph = Graph(tvm_cache)
+    tc = graph.get_transitive_closure(graph.get_topo())
+    seq_kernels = set()
+    for id in graph.get_topo():
+        for o in tc[id]:
+            tc[o].add(id)
+        if len(tc[id]) == graph.num_tvm_op:
+            seq_kernels.add(graph[id].func_name)
     
+    sk_metrics = defaultdict(lambda: defaultdict(lambda: 0))
+    for i in range(len(log_launch_table)):
+        kernel_name = log_launch_table.kernel_name[i]
+        metric_name = log_launch_table.metric_name[i]
+        unit = log_launch_table.unit[i]
+        value = log_launch_table.value[i]
+        A = 'Achieved Occupancy'
+        D = 'Duration'
+        if kernel_name[: len(kernel_name) - 8] in seq_kernels and metric_name in {A, D}:
+            if metric_name == D:
+                value = unit_trans[unit] * float(value)
+            sk_metrics[kernel_name][metric_name] = float(value)
+            sk_metrics[kernel_name]['dot'] = sk_metrics[kernel_name][A] * sk_metrics[kernel_name][D]
+    
+    dot_sum = 0
+    dur = 0
+    for kernel_name in sk_metrics:
+        dot_sum += sk_metrics[kernel_name]['dot']
+        dur += sk_metrics[kernel_name]['Duration']
+
+    res_json[res_entry]['fixed_achieved_occupancy(%)'] =  \
+        (res_json[res_entry]['achieved_occupancy(%)'] * 
+        res_json[res_entry]['duration(ms)'] - 
+        dot_sum) / (res_json[res_entry]['duration(ms)'] - dur) 
+
     s = json.dumps(res_json, indent=2)
     open(res_file, 'w').write(s)
 
